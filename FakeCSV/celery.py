9be4bb11465120bb343d22854a32b9526e22167c
abdulla bektayev <abdulla.bektayev@gmail.com>
2021-04-27 10:15:26 +0600

    connected reddis

--- FakeCSV/celery.py
+++ FakeCSV/celery.py
@@ -9,8 +9,3 @@ app = Celery('FakeCSV')
 app.conf.timezone = 'UTC'
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
-
-
-@app.task(bind=True)
-def debug_task(self):
-    print('Request: {0!r}'.format(self.request))
--- FakeCSV/settings.py
+++ FakeCSV/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ozbgb)%8t%403ulbs-$)_1_*n#)im#n9)77b&ubnrmjwtdw%=b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','0.0.0.0', '[::1]']
 
 
 # Application definition
