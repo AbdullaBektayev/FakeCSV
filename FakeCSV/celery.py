3c4e7339eb7e021b74f3f0409c9c41abaefcc456
abdulla bektayev <abdulla.bektayev@gmail.com>
2021-05-02 14:32:43 +0600

    corected celery

--- FakeCSV/celery.py
+++ FakeCSV/celery.py
@@ -2,10 +2,9 @@
 from __future__ import absolute_import
 import os
 from celery import Celery
-from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FakeCSV.settings')
 app = Celery('FakeCSV')
 app.conf.timezone = 'UTC'
 app.config_from_object("django.conf:settings", namespace="CELERY")
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app.autodiscover_tasks()
