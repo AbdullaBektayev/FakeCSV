778c39694e095785aeb339cc812c936a7cc94cea
abdulla bektayev <abdulla.bektayev@gmail.com>
2021-05-06 21:18:41 +0600

    static root

--- FakeCSV/settings.py
+++ FakeCSV/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOTS =
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [STATIC_DIR]
 
