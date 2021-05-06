b7e47134498fb36c805897986491b49366a35c5a
abdulla bektayev <abdulla.bektayev@gmail.com>
2021-05-06 21:16:10 +0600

    initial commit to heroku

--- FakeCSV/settings.py
+++ FakeCSV/settings.py
@@ -139,7 +139,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR,'static')
+# STATIC_ROOTS =
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [STATIC_DIR]
 
 
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn FakeCSV.wsgi
