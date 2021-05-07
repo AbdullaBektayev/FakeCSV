184523494953a2f2a72ccfcd0ef6dc78bc4c4cee
Abdulla <50593700+AbdullaBektayev@users.noreply.github.com>
2021-05-07 21:37:42 +0600

    Update settings.py
    deleted django key

--- FakeCSV/settings.py
+++ FakeCSV/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ozbgb)%8t%403ulbs-$)_1_*n#)im#n9)77b&ubnrmjwtdw%=b'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
